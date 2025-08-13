ce3b3e9687263d1f095914ad280ead70f6f8f9fa
Bovendiska <127710822+Bovendiska@users.noreply.github.com>
2025-08-13 10:31:37 +0700

    Update aset_saka.py

--- aset_saka.py
+++ aset_saka.py
@@ -14,21 +14,7 @@ SCOPE =[
 def submit_gsheet(data):
     try:
         # Memuat credentials dari st.secrets
-        
-        creds_dict = {
-            "type": "service_account",
-            "project_id": "streamlit-aset",
-            "private_key_id": "e9815b702481b331e280698d88e6bda83e68805d",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCt7K9jYb4mQGRB\nfocU6PP7tfVRQ5kUKjw9iIhFJw0DZiHWo9VKhDTgGHJdLVrIAGJFlMKz1c45hcBy\n1+AoP5Gp8lxLc8R+66mEjUzslJsU6eMlz+nQOjYlAuiMGa6SNDKH/0D1SD7Bp/dk\nKZRFvHjArKqZvK/9Pz6GPpInk2HhFTCe09vIEzaS0YyepJenhfsQ5lE+E1idVUN/\nTAcpXwKKJqD0xyPh9vJvX7ZMN3YaSd3rxArsHUOmkSLBtjw++iyq6Bs1zUyoqbsl\nf2HelKJm9Qu/glCnSJuphUHxm3inOkBjZu81/YTO4zk2ckGKF422X8ljpvZ2nxXV\naR0bAlutAgMBAAECggEAVgczL6y6sc/65xxvM4wQlspHiAUwNDW561Pb15y7xpkf\nlrwd9chUIR5mfyk+dJwf/7vX3L0eTvyWFxgpk67x3YZ4WYfvQzpM9Lgxuvm8hck0\nplDjnbAVdRQts5LGcyHndrZuyoSMKhzrdPtOEHprnh4qGnYm9NRyGFPd1j6D1SFj\nuFISRaDCR88Rbgt7SE0znE1qPlzYu7LeJn6FIWA4g0PpBv+Iv145pgn0n9MBssbp\nzcA21tVhttkn41kRtFlKC2Xa0rLPFFpMJ0F91utcrErCa1pyikwpHbstblmKm29N\nE7q+UaGAW0UlDbpEctc2s+UasSfAWnzlBwwzOAgtHwKBgQDzTId/X0zpmA4zokpd\n38uyIPKW0KHjPyy/s7EneiwlioEBjtlQhU2PQxnxb4/RpO/aVPKjZPBIed4C9qMP\n/8hn9hOkxjOA7tZJFMGSEzazr6csELfarwKfJgOPSMVhgcqefQlwSzRK131DyNni\nOewGXzd3W+2UqQY1LE64r7BlewKBgQC3AQeba2h4fyEkEtMdGGPSrjMdZq2zm/0a\n48z6BbvLPuyKj+Txeo+MLltInegRVMJJxjmNWO+PaU/Uprhs8QQYf45egss88WhS\n4wP0OJNtbvliPhhAzsuu1GU9Ly8TmLxwzHe84jQEP1aCWtlAH00BLociOOuXqwl5\n1dnRj5G29wKBgQCsRvZzJMJLbhYwTaynCD8uZNIiJJ1dUnCXVeANeh8kfgyXU9Vh\nhOLEnNkT8tE9u7LzQM+HD4RtbY3dP1N7kr0PkFmchZQWqggmO4JNszk6xxhyxWvK\nd749fydqIDdTshoNW7CBSV8/15KlmuVdoXIVmPqnk/qJF56DrFoZwJ4BXQKBgBTa\nfS9ssd7pPbGKo5vxJ47eYty60phg8hqaUsU62gdzCClR9FjACpOCxMwlkkhHTExz\n1iMRO1swOPSWevWPEVRpVKPYa6hqKeUoEU9HEyWpO6nCQalA51zovxCVy9uD0BVa\nd1qCvEKvpP/9sjvoVTIJR0TTD6Wcy9uiTsvefFBJAoGAcq4CRvlueJ5t8GuooJmT\nQcw+vgpEaWy+7PIvyQiFPM4K56Ovo+Oe2NNq6UZHp+UjlFv6oOOpzQi0BJYNxP7H\nGq/JJCFUKFqJ5pZA+sGZ8voczxJojdaOL+Kl4CsazSgtEpypZfq8U3ktXvrE7Ac0\nLzkWKfj2/U9d9iFNqpfHY2U=\n-----END PRIVATE KEY-----\n",
-            "client_email": "aset-device@streamlit-aset.iam.gserviceaccount.com",
-            "client_id": "117660315058839628089",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/aset-device%40streamlit-aset.iam.gserviceaccount.com",
-            "universe_domain": "googleapis.com"
-        }
-
+        creds_dict = st.secrets["gcp_service_account"].to_dict()
 
         creds = Credentials.from_service_account_info(creds_dict, scopes =SCOPE)
 
@@ -36,7 +22,7 @@ def submit_gsheet(data):
         client = gspread.authorize(creds)
 
         # Buka spreadsheet berdasarkan nama
-        gsheet = client.open("Aset_SAKA")
+        gsheet = client.open(st.secrets["gsheet_name"])
 
         # Pilih worksheet pertama
         worksheet = gsheet.get_worksheet(0)
